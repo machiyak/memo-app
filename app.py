63cc67b56ee9771c6d6c5f825279ddc1bf07385f
machiyak <day.by.day4692@icloud.com>
2026-05-08 22:56:42 +0900

    Add home page template

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# "/"にアクセスされたらindex()を呼び出す
 @app.route("/")
 def index():
-    return "Hello, World!"
+
+    # index.htmlをレンダリングする
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
