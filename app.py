832cd526752cf29769d0b3e78f601c2783c5a3ef
machiyak <day.by.day4692@icloud.com>
2026-05-09 16:44:05 +0900

    Add record list

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# memo記録用list
+memos = []
+
 # /というURLでGET, POSTの両方を受け付ける
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,9 +13,10 @@ def index():
     if request.method == "POST":
         # フォームの中身受け取る
         content = request.form["content"]
+        memos.append(content)
 
     # index.htmlをレンダリングする
-    return render_template("index.html", content=content)
+    return render_template("index.html", memos=memos)
 
 if __name__ == "__main__":
     app.run(debug=True)
