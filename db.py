a90a7b9c61262a08671a41a338878a8866408ea5
machiyak <day.by.day4692@icloud.com>
2026-05-09 21:42:21 +0900

    Add register page template

--- db.py
+++ db.py
@@ -15,6 +15,14 @@ CREATE TABLE IF NOT EXISTS memos (
 )
 """)
 
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL            
+)
+""")
+
 conn.commit()
 conn.close()
 
