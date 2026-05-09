9b795243e4a44706c6dbafb32a74a8224491f6c4
machiyak <day.by.day4692@icloud.com>
2026-05-09 16:37:07 +0900

    Add memo input form

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# "/"にアクセスされたらindex()を呼び出す
-@app.route("/")
+# /というURLでGET, POSTの両方を受け付ける
+@app.route("/", methods=["GET", "POST"])
 def index():
+    content = None
+
+    if request.method == "POST":
+        # フォームの中身受け取る
+        content = request.form["content"]
 
     # index.htmlをレンダリングする
-    return render_template("index.html")
+    return render_template("index.html", content=content)
 
 if __name__ == "__main__":
     app.run(debug=True)
