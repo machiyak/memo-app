ba3002bbcd9e30aa38d3fbbbe16e69f825d9bd29
machiyak <day.by.day4692@icloud.com>
2026-05-09 20:57:27 +0900

    Replace list with database

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
-# memo記録用list
-memos = []
+# memo用DB
+DB_NAME = "memo.db"
+
+# connを取得する
+def get_db_connection():
+    conn = sqlite3.connect(DB_NAME)
+    # 取得されるDBの型を辞書型にする
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 # /というURLでGET, POSTの両方を受け付ける
 @app.route("/", methods=["GET", "POST"])
 def index():
-    content = None
+    conn = get_db_connection()
 
     if request.method == "POST":
         # フォームの中身受け取る
         content = request.form["content"]
-        memos.append(content)
+        
+        conn.execute(
+            "INSERT INTO memos (content) VALUES (?)",
+            (content,)
+        )
+        conn.commit()
+        conn.close()
+        # フォーム再送信を防ぐために"/"にリダイレクト
+        return redirect(url_for("index"))
+
+    memos = conn.execute(
+        "SELECT * FROM  memos ORDER BY created_at DESC"
+    ).fetchall()
+
+    conn.close()
 
     # index.htmlをレンダリングする
     return render_template("index.html", memos=memos)
---
+++ db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+DB_NAME = "memo.db"
+
+conn = sqlite3.connect(DB_NAME)
+# DB操作用カーソル
+cur = conn.cursor()
+
+# メモを記録するためのmemosテーブルを作成し、メモのid, メモの内容, メモ作成日時の3つの列を作成
+cur.execute("""
+CREATE TABLE IF NOT EXISTS memos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEAULT CURRENT_TIMESTAMP
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database inisialized.")
