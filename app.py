421d854f59d524c1080bf889296158a898fcd91f
machiyak <day.by.day4692@icloud.com>
2026-05-12 15:11:59 +0900

    Add logout form

--- app.py
+++ app.py
@@ -101,5 +101,10 @@ def login():
     
     return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
 if __name__ == "__main__":
     app.run(debug=True)
